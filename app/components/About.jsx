29ffda23f5187db07f2d1dc79a9a3b52105b9c5c
Panibrat <a.panibratenko@gmail.com>
2016-11-06 19:33:38 +0200

    About page more changes

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,10 @@ var About = React.createClass({
 */
 var About =  (props) => {
         return (
-            <h3>About</h3>
-            <p>Welcome to about page</p>
+            <div>
+                <h3>About!</h3>
+                <p>Welcome to about page!</p>
+            </div>
         );
 };
 /*var About =  (props) =>  (<h3>About Component!</h3>);*/
